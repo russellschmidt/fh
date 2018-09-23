f625f7e8827bacd6b979eabd209d6b1135d9b619
Russell Schmidt <russell.schmidt@me.com>
2018-09-23 01:26:50 -0700

    basic styling

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -8,6 +8,29 @@ const Background = () => (
 		<div class={style.logo} >
 			<Logo />
 		</div>
+		<div>
+			<h1 class={style.h1}>Join FarmHaus</h1>
+		</div>
+		<div>
+			<h2 class={style.h2}>Enter your email to be in the know.</h2>
+		</div>
+		<div class={style.emailInput_container}>
+			<input type="email" placeholder="Enter your email" id="email-input" class={style.emailInput} name="email-input" />
+		</div>
+		<div class={style.btn_container}>
+			<button class={style.btn}>Enter</button>
+		</div>
+		<div class={style.socialIcon_container}>
+			<a href="https://www.fb.me/FarmHausApp" target="_blank" rel="noopener noreferrer" >
+				<img class={style.socialIcon} src="https://s3.amazonaws.com/farmhausapp/img/farmhaus-facebook%402x.png" alt="facebook icon" />
+			</a>
+			<a href="https://www.instagram.com/farmhausapp" target="_blank" rel="noopener noreferrer" >
+				<img class={style.socialIcon} src="https://s3.amazonaws.com/farmhausapp/img/farmhaus-instagram%402x.png" alt="instagram icon" />
+			</a>
+			<a href="https://www.twitter.com/farmhausapp" target="_blank" rel="noopener noreferrer" >
+				<img class={style.socialIcon} src="https://s3.amazonaws.com/farmhausapp/img/farmhaus-twitter%402x.png" alt="twitter icon" />
+			</a>
+		</div>
 	</div>
 );
 
