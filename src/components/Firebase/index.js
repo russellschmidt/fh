8a00b29f4f47121d8cba280718c5603bec4b2acc
Russell Schmidt <russell.schmidt@me.com>
2018-09-23 23:04:28 -0700

    add firebase with env vars hidden

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
-var config = {
-  apiKey: process.env.PREACT_APP_FIREBASE_API_KEY,
-  authDomain: "farmhausapp20180923.firebaseapp.com",
-  databaseURL: "https://farmhausapp20180923.firebaseio.com",
-  projectId: "farmhausapp20180923",
-  storageBucket: "",
-  messagingSenderId: "1096591295207"
+let config = {
+	apiKey: process.env.PREACT_APP_FIREBASE_API_KEY,
+	authDomain: 'farmhausapp20180923.firebaseapp.com',
+	databaseURL: 'https://farmhausapp20180923.firebaseio.com',
+	projectId: 'farmhausapp20180923',
+	storageBucket: '',
+	messagingSenderId: '1096591295207'
 };
 firebase.initializeApp(config);
 
