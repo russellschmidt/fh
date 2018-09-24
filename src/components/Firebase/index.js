75b416a186757da8a807984ba7135599cfe6589b
Russell Schmidt <russell.schmidt@me.com>
2018-09-23 22:55:12 -0700

    add env vars, add firebase library

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,2 +1,13 @@
 import firebase from 'firebase';
 
+var config = {
+  apiKey: process.env.PREACT_APP_FIREBASE_API_KEY,
+  authDomain: "farmhausapp20180923.firebaseapp.com",
+  databaseURL: "https://farmhausapp20180923.firebaseio.com",
+  projectId: "farmhausapp20180923",
+  storageBucket: "",
+  messagingSenderId: "1096591295207"
+};
+firebase.initializeApp(config);
+
+export default firebase;
