72530a91e6adad05cc1a37d67e7579ce05d44fb1
Russell Schmidt <russell.schmidt@me.com>
2018-09-30 23:34:57 -0700

    remove sendemail library and commented out the email.js file

---
+++ email.js
@@ -0,0 +1,19 @@
+// let sendemail   = require('sendemail').email; // no api key
+// let email = sendemail.email;
+// sendemail.set_template_directory('./src/templates');
+ 
+// const emailSend = () => {
+//   let person = {
+//     name: 'TEST',
+//     email: 'farmhaus.app@gmail.com', // person.email can also accept an array of emails
+//     subject: 'Welcome to FarmHaus.app!'
+//   };
+   
+//   email('welcome', person, (error, result) => {
+//     console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
+//     console.log(result);
+//     console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+//   });
+// }
+
+// export default emailSend;
